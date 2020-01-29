27b5c7675567b40b90722fc299bc54ce163c38cb
theyCallMeDudu <52638570+theyCallMeDudu@users.noreply.github.com>
2020-01-29 15:53:50 -0300

    1014 - Consumo

---
+++ 1014_consumo.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        int distancia;
+      	double preco, consumo;
+      
+      	Scanner scan = new Scanner(System.in);
+      
+        distancia = scan.nextInt();
+        preco = scan.nextDouble();
+        consumo = distancia * preco;
+      
+        System.out.println(String.format("%.3f", consumo) + " km/l");
+      
+    }
+ 
+}

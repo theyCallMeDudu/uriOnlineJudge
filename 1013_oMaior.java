a95d5ebc4dddfa5705861f965f0b4f713dfce7e0
theyCallMeDudu <52638570+theyCallMeDudu@users.noreply.github.com>
2020-01-29 10:33:55 -0300

    1013 - O maior

---
+++ 1013_oMaior.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+       int a, b, c, maiorAB;
+      
+       Scanner scan = new Scanner(System.in);
+			 
+       a = scan.nextInt(); 
+       b = scan.nextInt();
+       c = scan.nextInt();
+      
+       maiorAB = 
+         
+       if(maiorAB > c) {
+       		System.out.println(maiorAB + " eh o maior");  
+       } else {
+        	System.out.println(c + " eh o maior"); 
+       }
+      
+    }
+ 
+}

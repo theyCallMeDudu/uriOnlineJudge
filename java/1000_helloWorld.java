56970bb7222fcc2fe1dd2e99537ac2caa897268a
Eduardo Coelho <coelho.edu96@gmail.com>
2020-09-30 11:01:02 -0300

    Update 1000_helloWorld.java

--- java/1000_helloWorld.java
+++ java/1000_helloWorld.java
@@ -3,12 +3,6 @@ import java.io.IOException;
 public class Main {
  
     public static void main(String[] args) throws IOException {
- 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
         System.out.println("Hello World!");
     }
  

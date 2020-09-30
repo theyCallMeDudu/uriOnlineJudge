ef864d724a5180c89275c01d2764dc54c944cfb4
Eduardo Coelho <coelho.edu96@gmail.com>
2020-09-30 11:00:38 -0300

    Create 1000_helloWorld.java

---
+++ java/1000_helloWorld.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        System.out.println("Hello World!");
+    }
+ 
+}
